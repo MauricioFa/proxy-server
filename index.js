1657b4dda8862ae6d80f2822c0a58a2ae1f37919
Mauricio Fajardo <jmauriciofajardo@gmail.com>
2023-07-22 15:55:28 -0500

    initialize repository

---
+++ index.js
@@ -0,0 +1,35 @@
+const express = require("express");
+const { createProxyMiddleware } = require("http-proxy-middleware");
+require("dotenv").config();
+
+// Create Express Server
+const app = express();
+
+// Configuration
+const PORT = process.env.PORT || 3000;
+const HOST = process.env.HOST || "localhost";
+const API_SERVICE_URL = process.env.API_SERVICE_URL;
+const API_KEY = process.env.API_KEY;
+
+// Proxy endpoints
+app.use(
+  "/api",
+  createProxyMiddleware({
+    target: `${API_SERVICE_URL}`,
+    changeOrigin: true,
+    pathRewrite(pathReq, req) {
+      const pathname = pathReq.split("?")[0].replace("/api", "");
+      let url = `${pathname}?key=${API_KEY}`;
+      url = Object.entries(req.query).reduce(
+        (newUrl, [key, value]) => `${newUrl}&${key}=${encodeURI(value)}`,
+        url
+      );
+      return url;
+    },
+  })
+);
+
+// Start the Proxy
+app.listen(PORT, HOST, () => {
+  console.log(`Starting Proxy at ${HOST}:${PORT}`);
+});
